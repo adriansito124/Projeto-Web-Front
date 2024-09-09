f74f10713349a36b083eef23c470a00332bbf110
adriansito124 <adriangobara@gmail.com>
2024-09-09 11:53:57 -0300

    dark mode :<

--- pages/receitas/minhas-receitas/scripts/renderRecipes.js
+++ pages/receitas/minhas-receitas/scripts/renderRecipes.js
@@ -19,7 +19,7 @@ export async function renderRecipes() {
         
         div.insertAdjacentHTML("beforeend", `
             <div id="cards" class="row d-flex flex-wrap justify-content-center">
-                <div class="card d-flex flex-column align-items-center" style="width: 18rem;">
+                <div id="card" class="card d-flex flex-column align-items-center" style="width: 18rem;">
                     <img src="${src}" class="card-img-top" alt="...">
                     <div class="card-body d-flex flex-column align-items-center">
                         <h5 class="card-title">${element.name}</h5>
